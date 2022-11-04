f23f92081dac874cf9f2b467eff43e34335cec0a
bhagyac <bhagyac@Bhagyas-MacBook-Pro-2.local>
2022-11-04 12:17:17 -0400

    new tasks'

---
+++ html_php3.php
@@ -0,0 +1,33 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+<?php
+
+// print statements
+
+print "hello world"
+
+//comments
+/* multiline 
+comments 
+*/
+
+// Line breaks
+
+echo (" My name is Bhagya \r\n and I teach at Canada College");
+echo "I live in montreal";
+
+
+
+
+
+?>
+    
+</body>
+</html>
